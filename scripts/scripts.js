627dcfafcd23399f1b276b11f295cf94e065967b
tsern <tania.sernivka@gmail.com>
2018-09-14 21:53:45 +0300

    added google Map

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -38,6 +38,14 @@ function to_top(){
 		});
 }
 
+function initMap() {
+	var uluru = {lat: -25.344, lng: 131.036};
+	var map = new google.maps.Map(
+		document.getElementById('map'), {zoom: 4, center: uluru});
+	var marker = new google.maps.Marker({position: uluru, map: map});
+	
+}
+
 $(document).ready(function(){
 	isotope_init();
 	carousel_init();
