ae0be47ab8bb1ceda5c254c41f0f5d8a71cb6a29
tsern <tania.sernivka@gmail.com>
2018-09-16 22:29:01 +0300

    fixed map section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,15 +39,207 @@ function to_top(){
 }
 
 function initMap() {
-	var uluru = {lat: -25.344, lng: 131.036};
+	var uluru = {lat: 49.8382600, lng: 24.0232400};
 	var map = new google.maps.Map(
-		document.getElementById('map'), {zoom: 4, center: uluru});
+		document.getElementById('map'), {
+			zoom: 12, 
+			center: uluru,
+			styles: [
+				{
+					"featureType": "all",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"weight": "2.00"
+						}
+					]
+				},
+				{
+					"featureType": "all",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+							"color": "#9c9c9c"
+						}
+					]
+				},
+				{
+					"featureType": "all",
+					"elementType": "labels.text",
+					"stylers": [
+						{
+							"visibility": "on"
+						}
+					]
+				},
+				{
+					"featureType": "landscape",
+					"elementType": "all",
+					"stylers": [
+						{
+							"color": "#f2f2f2"
+						}
+					]
+				},
+				{
+					"featureType": "landscape",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						}
+					]
+				},
+				{
+					"featureType": "landscape.man_made",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "all",
+					"stylers": [
+						{
+							"saturation": -100
+						},
+						{
+							"lightness": 45
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"color": "#eeeeee"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#7b7b7b"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "simplified"
+						}
+					]
+				},
+				{
+					"featureType": "road.arterial",
+					"elementType": "labels.icon",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "transit",
+					"elementType": "all",
+					"stylers": [
+						{
+							"visibility": "off"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "all",
+					"stylers": [
+						{
+							"color": "#46bcec"
+						},
+						{
+							"visibility": "on"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+							"color": "#c8d7d4"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#070707"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"color": "#ffffff"
+						}
+					]
+				}
+			]
+			
+		});
 	var marker = new google.maps.Marker({position: uluru, map: map});
 	
+}
+
+/*Map overlay script*/
+function mapOverlatHidden(){
+	var mapTitle = $('.map-title'),
+		mapOverlay = $('.map-overlay'),
+		sectionMap = $('.map-section');
+
+		mapTitle.click(function(){
+			mapOverlay.addClass('hidden');
+		});
+
+		$(document).click(function(e){
+			if ((mapTitle.has(e.target).length === 0) && (sectionMap.has(e.target).length !==1)){
+					mapOverlay.removeClass('hidden');
+				};
+		});
 }
 
 $(document).ready(function(){
 	isotope_init();
 	carousel_init();
 	to_top();
+	mapOverlatHidden();
 });
